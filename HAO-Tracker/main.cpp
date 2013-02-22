0957740f8a1b86d5a0be44d68ec6c5e79a404eac
sebastienjean <baz.jean@gmail.com>
2013-02-22 16:10:56 +0100

    Renamed defs.h to pins.h, renamed fields and constants
    - changed some constants and fields names to more explicit ones

--- HAO-Tracker/main.cpp
+++ HAO-Tracker/main.cpp
@@ -18,51 +18,51 @@
 #include <GPS.h>
 #include <SoftwareSerial.h>
 #include <FSK600BaudTA900TB1500Mod.h>
-#include <defs.h>
+#include <pins.h>
 
 // SD logfile path
 #define LOGFILE "data.txt"
 
-#define SENSOR_STRING_LENGTH 40
+#define SENSOR_DATA_ASCII_STRING_LENGTH 40
 
 #define KIWI_FRAME_LENGTH 11
 
 // FSK modulator
-FSK600BaudTA900TB1500Mod fskMod(FSK_MOD_TX);
+FSK600BaudTA900TB1500Mod fskModulator(FSK_MODULATOR_TX);
 
 // Software serial link used by GPS
 #if defined(GPS_SERIAL_RX)
-SoftwareSerial nmeaSerial(GPS_SERIAL_RX, GPS_SERIAL_TX);
+SoftwareSerial serialNmeaGPSPort(GPS_SERIAL_RX, GPS_SERIAL_TX);
 #else
-#define nmeaSerial Serial1
+#define serialNmeaGPSPort Serial1
 #endif
 
 // GPS
-GPS nmea(&nmeaSerial, 2000, 2000);
+GPS serialNmeaGPS(&serialNmeaGPSPort, 2000,2000);
 
 // sensor data, as ASCII
-char sensorString[SENSOR_STRING_LENGTH];
+char sensorString[SENSOR_DATA_ASCII_STRING_LENGTH];
 
 // log File
 File logFile;
 
 // absolute pressure sensor value
-int absValue = 0;
+int absolutePressureSensorValue = 0;
 
 // differential pressure sensor value
-int diffValue = 0;
+int differentialPressureSensorValue = 0;
 
 // internal temperature sensor value
-int tempInValue = 0;
+int internalTemperatureSensorValue = 0;
 
 // external temperature sensor value
-int tempOutValue = 0;
+int externalTemperatureSensorValue = 0;
 
 // battery voltage sensor value
-int voltageValue = 0;
+int batteryVoltageSensorValue = 0;
 
 // buffer for NMEA sentence reading
-char nmeaFrame[MAX_NMEA_SENTENCE_LENGTH];
+char nmeaSentence[MAX_NMEA_SENTENCE_LENGTH];
 
 unsigned char kiwiFrame[KIWI_FRAME_LENGTH];
 
@@ -89,20 +89,20 @@ void
 setup()
 {
   // LEDs init
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_ORANGE, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(ORANGE_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
 
-  digitalWrite(LED_RED, HIGH);
-  digitalWrite(LED_ORANGE, HIGH);
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_BLUE, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(ORANGE_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(BLUE_LED, HIGH);
   delay(1000);
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_ORANGE, LOW);
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_BLUE, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(ORANGE_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 
   pinMode(USER_BUTTON, INPUT);
   digitalWrite(USER_BUTTON, HIGH);
@@ -112,16 +112,16 @@ setup()
 
   // SD card init
   Serial.print(F("SD Init..."));
-  pinMode(SD_CS, OUTPUT);
-  if (!SD.begin(SD_CS))
+  pinMode(SD_CARD_CHIP_SELECT, OUTPUT);
+  if (!SD.begin(SD_CARD_CHIP_SELECT))
     {
       Serial.println(F("KO"));
-      digitalWrite(LED_RED,HIGH);
+      digitalWrite(RED_LED,HIGH);
     }
   else
     {
       Serial.println(F("OK"));
-      digitalWrite(LED_GREEN,HIGH);
+      digitalWrite(GREEN_LED,HIGH);
       delay(1000);
       if (digitalRead(USER_BUTTON) == 0)
         {
@@ -130,9 +130,9 @@ setup()
           SD.remove(LOGFILE);
           for (int i=0;i<5;i++)
             {
-              digitalWrite(LED_ORANGE, HIGH);
+              digitalWrite(ORANGE_LED, HIGH);
               delay(100);
-              digitalWrite(LED_ORANGE, LOW);
+              digitalWrite(ORANGE_LED, LOW);
               delay(100);
             }
         }
@@ -146,9 +146,9 @@ setup()
           Serial.println(F("OK"));
           for (int i=0;i<5;i++)
             {
-              digitalWrite(LED_GREEN, HIGH);
+              digitalWrite(GREEN_LED, HIGH);
               delay(100);
-              digitalWrite(LED_GREEN, LOW);
+              digitalWrite(GREEN_LED, LOW);
               delay(100);
             }
         }
@@ -158,9 +158,9 @@ setup()
           Serial.println(F("KO"));
           for (int i=0;i<5;i++)
             {
-              digitalWrite(LED_RED, HIGH);
+              digitalWrite(RED_LED, HIGH);
               delay(100);
-              digitalWrite(LED_RED, LOW);
+              digitalWrite(RED_LED, LOW);
               delay(100);
             }
         }
@@ -168,7 +168,7 @@ setup()
     }
 
       // GPS on software serial at 4800 Baud
-  nmeaSerial.begin(4800);
+  serialNmeaGPSPort.begin(4800);
 
   for (int i = 0; i < 11; i++)
     kiwiFrame[i] = 0x00;
@@ -192,51 +192,51 @@ loop()
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = ',';
   // absolute pressure processing
-  absValue = analogRead(ABS_P);
-  itoa(absValue, sensorString + sensorStringOffset, 10);
+  absolutePressureSensorValue = analogRead(ABSOLUTE_PRESSURE_ANALOG_SENSOR);
+  itoa(absolutePressureSensorValue, sensorString + sensorStringOffset, 10);
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = ',';
-  kiwiFrame[1] = (unsigned char) (absValue / 4);
+  kiwiFrame[1] = (unsigned char) (absolutePressureSensorValue / 4);
   if (kiwiFrame[1] == 0xFF)
     kiwiFrame[1] = 0xFE;
 
   // differential pressure processing
-  diffValue = analogRead(DIFF_P);
-  itoa(diffValue, sensorString + sensorStringOffset, 10);
+  differentialPressureSensorValue = analogRead(DIFFERENTIAL_PRESSURE_ANALOG_SENSOR);
+  itoa(differentialPressureSensorValue, sensorString + sensorStringOffset, 10);
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = ',';
-  kiwiFrame[2] = (unsigned char) (diffValue / 4);
+  kiwiFrame[2] = (unsigned char) (differentialPressureSensorValue / 4);
   if (kiwiFrame[2] == 0xFF)
     kiwiFrame[2] = 0xFE;
 
   // internal temperature pressure processing
-  tempInValue = analogRead(TEMPIN);
-  itoa(tempInValue, sensorString + sensorStringOffset, 10);
+  internalTemperatureSensorValue = analogRead(INTERNAL_TEMPERATURE_ANALOG_SENSOR);
+  itoa(internalTemperatureSensorValue, sensorString + sensorStringOffset, 10);
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = ',';
-  kiwiFrame[3] = (unsigned char) (tempInValue / 4);
+  kiwiFrame[3] = (unsigned char) (internalTemperatureSensorValue / 4);
   if (kiwiFrame[3] == 0xFF)
     kiwiFrame[3] = 0xFE;
 
   // external temperature pressure processing
-  tempOutValue = analogRead(TEMPOUT);
-  itoa(tempOutValue, sensorString + sensorStringOffset, 10);
+  externalTemperatureSensorValue = analogRead(EXTERNAL_TEMPERATURE_ANALOG_SENSOR);
+  itoa(externalTemperatureSensorValue, sensorString + sensorStringOffset, 10);
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = ',';
-  kiwiFrame[4] = (unsigned char) (tempOutValue / 4);
+  kiwiFrame[4] = (unsigned char) (externalTemperatureSensorValue / 4);
   if (kiwiFrame[4] == 0xFF)
     kiwiFrame[4] = 0xFE;
 
   // battery voltage processing
-  voltageValue = analogRead(VOLTAGE);
-  itoa(voltageValue, sensorString + sensorStringOffset, 10);
+  batteryVoltageSensorValue = analogRead(BATTERY_VOLTAGE_ANALOG_SENSOR);
+  itoa(batteryVoltageSensorValue, sensorString + sensorStringOffset, 10);
   sensorStringOffset = strlen(sensorString);
   sensorString[sensorStringOffset++] = '\r';
   sensorString[sensorStringOffset++] = '\n';
-  kiwiFrame[5] = (unsigned char) (voltageValue / 4);
+  kiwiFrame[5] = (unsigned char) (batteryVoltageSensorValue / 4);
   if (kiwiFrame[5] == 0xFF)
     kiwiFrame[5] = 0xFE;
-  kiwiFrame[9] = (unsigned char) (voltageValue / 8);
+  kiwiFrame[9] = (unsigned char) (batteryVoltageSensorValue / 8);
 
   for (int cpt = 1; cpt < KIWI_FRAME_LENGTH - 1; cpt++)
     chk = (unsigned char) ((chk + kiwiFrame[cpt]) % 256);
@@ -246,34 +246,34 @@ loop()
 
   // Kiwi Frame transmission
   for (int cpt = 0; cpt < KIWI_FRAME_LENGTH; cpt++)
-    fskMod.write(kiwiFrame[cpt]);
-  fskMod.off();
+    fskModulator.write(kiwiFrame[cpt]);
+  fskModulator.off();
 
   // Logging
   logFile = SD.open(LOGFILE, FILE_WRITE);
   if (logFile)
     {
       Serial.println(F("log file access success"));
-      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(GREEN_LED, HIGH);
       delay(100);
-      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(GREEN_LED, LOW);
       delay(100);
-      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(GREEN_LED, HIGH);
       delay(100);
-      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(GREEN_LED, LOW);
       logFile.print(sensorString);
       logFile.close();
     }
   else
     {
       Serial.println(F("log file access failure"));
-      digitalWrite(LED_RED, HIGH);
+      digitalWrite(RED_LED, HIGH);
       delay(100);
-      digitalWrite(LED_RED, LOW);
+      digitalWrite(RED_LED, LOW);
       delay(100);
-      digitalWrite(LED_RED, HIGH);
+      digitalWrite(RED_LED, HIGH);
       delay(100);
-      digitalWrite(LED_RED, LOW);
+      digitalWrite(RED_LED, LOW);
     }
       //wdt_reset();
 
@@ -284,118 +284,118 @@ loop()
 
   // Transmission
   for (int cpt = 0; cpt < strlen(sensorString); cpt++)
-    fskMod.write(sensorString[cpt]);
-  fskMod.off();
+    fskModulator.write(sensorString[cpt]);
+  fskModulator.off();
 
   // NMEA RMC
-  gpsStatus = nmea.readRMC(nmeaFrame);
+  gpsStatus = serialNmeaGPS.readRMC(nmeaSentence);
   switch (gpsStatus)
     {
   case GPS_OK:
     for (int cpt = 0; cpt < MAX_NMEA_SENTENCE_LENGTH; cpt++)
       {
-        if ((cpt > 5) && (cpt <= 25) && (nmeaFrame[cpt] == 'A'))
+        if ((cpt > 5) && (cpt <= 25) && (nmeaSentence[cpt] == 'A'))
           {
-            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(ORANGE_LED, HIGH);
             delay(100);
-            digitalWrite(LED_ORANGE, LOW);
+            digitalWrite(ORANGE_LED, LOW);
             delay(100);
-            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(ORANGE_LED, HIGH);
             delay(100);
-            digitalWrite(LED_ORANGE, LOW);
+            digitalWrite(ORANGE_LED, LOW);
           }
       }
     break;
   case GPS_TIMEOUT:
-    strcpy(nmeaFrame, "GPS TO\r\n");
+    strcpy(nmeaSentence, "GPS TO\r\n");
     break;
   default:
-    strcpy(nmeaFrame, "GPS E\r\n");
+    strcpy(nmeaSentence, "GPS E\r\n");
     break;
     }
   //wdt_reset();
 
   // Debug
-  Serial.print(nmeaFrame);
+  Serial.print(nmeaSentence);
 
   // Transmission
-  for (int cpt = 0; cpt < strlen(nmeaFrame); cpt++)
-    fskMod.write(nmeaFrame[cpt]);
-  fskMod.off();
+  for (int cpt = 0; cpt < strlen(nmeaSentence); cpt++)
+    fskModulator.write(nmeaSentence[cpt]);
+  fskModulator.off();
 
   // Logging
   logFile = SD.open(LOGFILE, FILE_WRITE);
   if (logFile)
     {
       Serial.println(F("log file access success"));
-      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(GREEN_LED, HIGH);
       delay(100);
-      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(GREEN_LED, LOW);
       delay(100);
-      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(GREEN_LED, HIGH);
       delay(100);
-      digitalWrite(LED_GREEN, LOW);
-      logFile.print(nmeaFrame);
+      digitalWrite(GREEN_LED, LOW);
+      logFile.print(nmeaSentence);
       logFile.close();
     }
   else
     {
       Serial.println(F("log file access failure"));
-      digitalWrite(LED_RED, HIGH);
+      digitalWrite(RED_LED, HIGH);
       delay(100);
-      digitalWrite(LED_RED, LOW);
+      digitalWrite(RED_LED, LOW);
       delay(100);
-      digitalWrite(LED_RED, HIGH);
+      digitalWrite(RED_LED, HIGH);
       delay(100);
-      digitalWrite(LED_RED, LOW);
+      digitalWrite(RED_LED, LOW);
     }
   // NMEA RMC
-  gpsStatus = nmea.readGGA(nmeaFrame);
+  gpsStatus = serialNmeaGPS.readGGA(nmeaSentence);
   switch (gpsStatus)
     {
   case GPS_OK:
     break;
   case GPS_TIMEOUT:
-    strcpy(nmeaFrame, "GPS TO\r\n");
+    strcpy(nmeaSentence, "GPS TO\r\n");
     break;
   default:
-    strcpy(nmeaFrame, "GPS E\r\n");
+    strcpy(nmeaSentence, "GPS E\r\n");
     break;
     }
 
   // Debug
-  Serial.print(nmeaFrame);
+  Serial.print(nmeaSentence);
 
   // Transmission
-  for (int cpt = 0; cpt < strlen(nmeaFrame); cpt++)
-    fskMod.write(nmeaFrame[cpt]);
-  fskMod.off();
+  for (int cpt = 0; cpt < strlen(nmeaSentence); cpt++)
+    fskModulator.write(nmeaSentence[cpt]);
+  fskModulator.off();
 
   // Logging
     logFile = SD.open(LOGFILE, FILE_WRITE);
     if (logFile)
       {
         Serial.println(F("log file access success"));
-        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(GREEN_LED, HIGH);
         delay(100);
-        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(GREEN_LED, LOW);
         delay(100);
-        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(GREEN_LED, HIGH);
         delay(100);
-        digitalWrite(LED_GREEN, LOW);
-        logFile.print(nmeaFrame);
+        digitalWrite(GREEN_LED, LOW);
+        logFile.print(nmeaSentence);
         logFile.close();
       }
     else
       {
         Serial.println(F("log file access failure"));
-        digitalWrite(LED_RED, HIGH);
+        digitalWrite(RED_LED, HIGH);
         delay(100);
-        digitalWrite(LED_RED, LOW);
+        digitalWrite(RED_LED, LOW);
         delay(100);
-        digitalWrite(LED_RED, HIGH);
+        digitalWrite(RED_LED, HIGH);
         delay(100);
-        digitalWrite(LED_RED, LOW);
+        digitalWrite(RED_LED, LOW);
       }
   //wdt_reset();
 }
---
+++ HAO-Tracker/platforms/standard/pins.h
@@ -0,0 +1,39 @@
+/*
+ Copyright (C) 2012 Sebastien Jean <baz dot jean at gmail dot com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the version 3 GNU General Public License as
+ published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define USER_BUTTON 5
+
+#define RED_LED 9
+
+#define ORANGE_LED 10
+
+#define GREEN_LED 7
+
+#define BLUE_LED 6
+
+#define FSK_MODULATOR_TX 6
+
+#define DIFFERENTIAL_PRESSURE_ANALOG_SENSOR A0
+
+#define ABSOLUTE_PRESSURE_ANALOG_SENSOR A1
+
+#define EXTERNAL_TEMPERATURE_ANALOG_SENSOR A2
+
+#define INTERNAL_TEMPERATURE_ANALOG_SENSOR A3
+
+#define BATTERY_VOLTAGE_ANALOG_SENSOR A4
+
+#define SD_CARD_CHIP_SELECT 8
