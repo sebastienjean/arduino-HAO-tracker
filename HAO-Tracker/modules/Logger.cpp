91d1563a4709da86e840b6b52ccb09c08a194607
sebastienjean <baz.jean@gmail.com>
2013-03-15 08:08:04 +0100

    Cleaned #include in Logger and Led modules
    - removed useless #include

--- HAO-Tracker/modules/Logger.cpp
+++ HAO-Tracker/modules/Logger.cpp
@@ -14,9 +14,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <Arduino.h>
-#include <SD.h>
 #include <pins.h>
-#include <defs.h>
+#include <SD.h>
 
 #include <Logger.h>
 
--- HAO-Tracker/modules/led.cpp
+++ HAO-Tracker/modules/led.cpp
@@ -14,8 +14,6 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <Arduino.h>
-#include <pins.h>
-#include <defs.h>
 
 #include <led.h>
 
