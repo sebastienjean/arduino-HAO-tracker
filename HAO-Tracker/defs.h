71eb6b976999f309d21416c488d87667ea2b08b8
sebastienjean <baz.jean@gmail.com>
2013-03-16 13:28:46 +0100

    Fixed comments, applied GNU code style to defs.h

--- HAO-Tracker/defs.h
+++ HAO-Tracker/defs.h
@@ -21,21 +21,45 @@
 #include <AnalogSensors.h>
 #include <Counters.h>
 
+/**
+ * Serial used for debug
+ */
 #define SERIAL_DEBUG Serial
 
+/**
+ * GPS serial baudrate
+ */
 #define SERIAL_NMEA_GPS_BAUDRATE 4800
 
+/**
+ * GPS reading timeout, in milliseconds
+ */
 #define SERIAL_NMEA_GPS_READING_MILLIS_TIMEOUT 2000
 
+/**
+ * GPS reading timeout, in characters
+ */
 #define SERIAL_NMEA_GPS_READING_CHARS_TIMEOUT 2000
 
+/**
+ * Log file path
+ */
 #define LOG_FILE_PATH "data.txt"
 
+/**
+ * HAO name
+ */
 #define HAO_NAME "POKEBALL"
 
+/**
+ * Frame counter EEPROM base address
+ */
 #define FRAME_COUNTER_BASE_ADDRESS 0x0000
+
+/**
+ * Reset counter EEPROM base address
+ */
 #define RESET_COUNTER_BASE_ADDRESS 0x0002
 
 #endif
-
 
