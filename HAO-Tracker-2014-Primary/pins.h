06bc3ca21067aa4d98cff2a78f939bac136574d0
sebastienjean <baz.jean@gmail.com>
2014-04-09 09:51:05 +0200

    [HAO-Tracker-Primary] commented pins.h

--- HAO-Tracker-2014-Primary/pins.h
+++ HAO-Tracker-2014-Primary/pins.h
@@ -16,26 +16,84 @@
 #ifndef PINS_h
 #define PINS_h
 
+/**
+ * User switch, used during reset to clear counters and erase SD file content
+ */
 #define USER_SWITCH_PIN 5
+
+/**
+ * Switch used to notify that taking-off is close
+ */
 #define TAKE_OFF_SWITCH_PIN 12
 
+/**
+ * output pin for FSK modulated signal
+ */
 #define FSK_MODULATOR_TX_PIN 6
 
+/**
+ * Internal temperature sensor analog channel
+ */
 #define INTERNAL_TEMPERATURE_ANALOG_SENSOR_CHANNEL A0
+
+/**
+ * Differential pressure sensor analog channel
+ */
 #define DIFFERENTIAL_PRESSURE_ANALOG_SENSOR_CHANNEL A1
+
+/**
+ * Battery voltage sensor analog channel
+ */
 #define BATTERY_VOLTAGE_ANALOG_SENSOR_CHANNEL A2
+
+/**
+ * Battery temperature sensor analog channel
+ */
 #define BATTERY_TEMPERATURE_ANALOG_SENSOR_CHANNEL A3
+
+/**
+ * X-axis acceleration sensor analog channel
+ */
 #define ACCELERATION_X_ANALOG_SENSOR_CHANNEL A4
+
+/**
+ * Y-axis acceleration sensor analog channel
+ */
 #define ACCELERATION_Y_ANALOG_SENSOR_CHANNEL A5
+
+/**
+ * Z-axis acceleration sensor analog channel
+ */
 #define ACCELERATION_Z_ANALOG_SENSOR_CHANNEL A6
 
+/**
+ * SD card chip-select pin
+ */
 #define SD_CARD_CHIP_SELECT_PIN 53
 
-#define RTC_CE_PIN   7
-#define RTC_IO_PIN   9
+/**
+ * RealTime Clock chip enable pin
+ */
+#define RTC_CE_PIN 7
+
+/**
+ * RealTime Clock IO pin
+ */
+#define RTC_IO_PIN 9
+
+/**
+ * RealTime Clock SCLK pin
+ */
 #define RTC_SCLK_PIN 10
 
+/**
+ * White LED pin
+ */
 #define LED_WHITE 11
-#define LED_GREEN 08
+
+/**
+ * Green LED pin
+ */
+#define LED_GREEN 8
 
 #endif
